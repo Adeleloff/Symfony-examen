8014b36f06952263517a778a7b5b5212ad48cf9d
Adele Jausons <adelejl@yahoo.fr>
2024-09-09 20:01:15 +0200

    suppression easyadmin

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     TalesFromADev\Twig\Extra\Tailwind\Bridge\Symfony\Bundle\TalesFromADevTwigExtraTailwindBundle::class => ['all' => true],
     TalesFromADev\FlowbiteBundle\TalesFromADevFlowbiteBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
